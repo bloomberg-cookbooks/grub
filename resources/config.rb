8c39f1a77a2103d70f67e1825bc6c343bcb00451
Stuart Sears <stuart@sjsears.com>
2020-05-22 09:39:21 +0100

    Lint fixes from PR comments

--- resources/config.rb
+++ resources/config.rb
@@ -12,22 +12,22 @@ property :path, String, name_property: true
 property :settings, Hash, default: {}
 
 def merge_args(arghash)
-    out = []
-    arghash.each do |key, val|
-      # if a value is an array return key=val for each val in v
-      # allows multiple args like console= in kernel boot parameters
-      if val.is_a?(Array)
-        val.each do |v|
-          out << "#{key}=#{v}"
-        end
-      elsif val.nil?
-        # if value is null, just return the key (effectively a boolean flag)
-        out << key.to_s
-      else
-        out << "#{key}=#{val}"
+  out = []
+  arghash.each do |key, val|
+    # if a value is an array return key=val for each val in v
+    # allows multiple args like console= in kernel boot parameters
+    if val.is_a?(Array)
+      val.each do |v|
+        out << "#{key}=#{v}"
       end
+    elsif val.nil?
+      # if value is null, just return the key (effectively a boolean flag)
+      out << key.to_s
+    else
+      out << "#{key}=#{val}"
     end
-    out.join(' ')
+  end
+  out.join(' ')
 end
 
 def content
@@ -35,9 +35,7 @@ def content
     next n.flatten.map(&:to_s).join(',') if n.is_a?(Array)
     n
   end.map do |k, v|
-    if v.is_a?(Hash)
-      v = merge_args(v)
-    end
+    v = merge_args(v) if v.is_a?(Hash)
     'GRUB_' << k.upcase << ((v.is_a?(String) && v =~ /[\W\s]+/) ? "=\"#{v}\"" : "=#{v}")
   end.join("\n")
 end
